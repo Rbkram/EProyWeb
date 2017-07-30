3ff93f8aecc06f84edfd812c9ef89e35bd6073ec
Rebeca <rramirezb1@ucenfotec.ac.cr>
2017-07-29 19:11:27 -0600

    hacer e¡vista del nav en includes y agregarla en vista de landing y registro

--- public/components/players/player.controller.js
+++ public/components/players/player.controller.js
@@ -8,17 +8,16 @@
     function playerCtrl($scope, $location){ //se inyecta el service userService en el controlador para que se tenga acceso
       //controlador
       var vm = this; //binding del controlador con el html, solo en el controlador
+      vm.selectedIndex = 0;
 
       function init(){ // función que se llama así misma para indicar que sea lo primero que se ejecute
         
        }init();
 
-       // vm.logIn = function(event){
+       // vm.buy = function(event){
        //  event.preventDefault();
-       //  $location.path('/login');
+       //  $location.path('/buyProperties');
        // }
-
-       
 
     }
 
